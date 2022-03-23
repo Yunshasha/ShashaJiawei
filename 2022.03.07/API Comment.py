6c02b926ff3d0fc821875689e93a18326bb54521
invincible1215 <66477449+invincible1215@users.noreply.github.com>
2022-03-23 06:57:14 +0100

    Create API Comment.py

---
+++ 2022.03.07/API Comment.py
@@ -0,0 +1,25 @@
+## API comments
+
+For example:
+
+``
+import cdsapi #import API
+
+c = cdsapi.Client() #call API variable
+
+c.retrieve(
+    'cams-europe-air-quality-forecasts', #name of dataset
+    {
+        'model': 'ensemble',
+        'date': '2022-03-19/2022-03-21', #for time period
+        'format': 'netcdf',
+        'variable': 'ozone',
+        'level': '0',
+        'type': 'analysis',
+        'time': [
+            '00:00', '23:00',
+        ], #for multiple choices
+        'leadtime_hour': '0',
+    }, #all the check boxes and options in each dataset, written in dictionary format in API method
+    'download.nc' #the downloaded file name)
+``
